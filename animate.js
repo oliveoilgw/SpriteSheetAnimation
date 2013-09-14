9b85a4ecde3050850ec989073bb8e0c9a98d8177
Grace Wu <oliveoilgw@gmail.com>
2013-09-13 23:36:33 -0700

    added animate.js
    animateSprites animate a set of sprits in a spritsheets. the spritsheet
    should be constructed by m x n sprits with fixed width and height.

---
+++ animate.js
@@ -0,0 +1,41 @@
+﻿(function ($) {
+    $.fn.animateSprites = function (options) {
+
+        return this.each(function () {
+
+            var settings = $.extend({
+                interval: 200,
+                tiles: 1        //number of tiles.
+            }, options);
+
+            var $this = $(this);        //$this is the canvas object;
+            var ctx = this.getContext("2d");
+            var $img = $(options.source);
+            if (!$img.is('img')) return;  //source is not an image;
+            var img = $img[0];
+            var tcount = 1;
+            var width = options.width;
+            var height = options.height;
+
+            var sprite = function () {
+                setTimeout(function () {
+                    if (tcount > options.tiles) return;
+                    ctx.clearRect(0, 0, width, height);
+                    var corx = Math.floor(tcount / 4);
+                    var cory = tcount % 4;
+                    draw(corx, cory, width, height);
+                    //console.log(tcount, corx, cory);
+                    tcount++;
+                    sprite();
+                }, settings.interval);      //draw image on canvas to form animation every interval(ms);
+            }
+
+            function draw(corx, cory, width, height) {
+                ctx.drawImage(img, corx * width, cory * height, width, height, 0, 0, width, height);
+            }
+
+            sprite();
+        });
+    }
+})(jQuery);
+
